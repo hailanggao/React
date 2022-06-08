aaa14e72de4e9b9962ccda7286401cf337dd8f39
Gordon Gao <hailanggao@outlook.com>
2022-06-08 08:46:09 +0800

    change to JSX using Babel transformer

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const reactContentRoot = document.getElementById("root")
-const myFirstELement = React.createElement(
-    "ul",
-    null,
-    [React.createElement('li', null, 'item1'), 
-    React.createElement('li', null, 'item2'),
-    React.createElement('li', null, 'item3')]
-    )
+// const myFirstELement = React.createElement(
+//     "ul",
+//     null,
+//     [React.createElement('li', null, 'item1'), 
+//     React.createElement('li', null, 'item2'),
+//     React.createElement('li', null, 'item3')]
+//     )
 
-ReactDOM.render(myFirstELement, reactContentRoot)
+const myJSXElement = (
+    <ul>
+        <li>item1</li>
+        <li>item2</li>
+    </ul>
+
+)
+
+ReactDOM.render(myJSXElement, reactContentRoot)
