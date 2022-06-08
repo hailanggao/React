44eb1536351237d05d22c0fad68d211e1ee64b9b
Gordon Gao <hailanggao@outlook.com>
2022-06-08 11:21:05 +0800

    react component lifecycle

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,24 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './components/App'
 
-const reactContentRoot = document.getElementById("root")
-// const myFirstELement = React.createElement(
-//     "ul",
-//     null,
-//     [React.createElement('li', null, 'item1'), 
-//     React.createElement('li', null, 'item2'),
-//     React.createElement('li', null, 'item3')]
-//     )
-
-const App = () =>{
-    const myItem = "Gordon"
-    return(
-        <ul>
-            <li>item1</li>
-            <li>item2</li>
-            <li>{myItem.toUpperCase()}</li>
-        </ul>
-    )
-}
-
-ReactDOM.render(<App />, reactContentRoot)
+ReactDOM.render(<App />, document.getElementById("root"))
