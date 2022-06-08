2090f678ec3fca0b559e4f0cccd3f75a5b3b905d
Gordon Gao <hailanggao@outlook.com>
2022-06-08 13:18:16 +0800

    onChange event

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react"
 import CountButton from "./CountButton/CountButton"
+import SearchBar from "./SearchBar/SearchBar"
 const App = () =>{
     return(
         <div>
             <CountButton incrementBy={1} buttonColor={"blue"} borderRadius={"2px"}/>
             <CountButton incrementBy={5} buttonColor={"red"} borderRadius={"10px"}/>
+            <SearchBar />
         </div>
     )
 }
---
+++ my-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+import './SearchBar.css'
+
+const SearchBar=()=>{
+    const [searchValue, setSearchValue] = useState("the search value")
+
+    const handleInputChange = (event) => {
+        setSearchValue(event.target.value)
+    }
+    return <div>
+        <input type="text" value={searchValue} onChange={handleInputChange}/>
+    </div>
+}
+
+export default SearchBar
