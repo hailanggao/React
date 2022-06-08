d6dbaa55da9f067fc6f6e6aa92d35376994ad818
Gordon Gao <hailanggao@outlook.com>
2022-06-08 13:40:26 +0800

    conditional rendering

--- my-app/src/components/SearchBar/SearchBar.js
+++ my-app/src/components/SearchBar/SearchBar.js
@@ -2,13 +2,22 @@ import React, {useState} from 'react'
 import './SearchBar.css'
 
 const SearchBar=()=>{
-    const [searchValue, setSearchValue] = useState("the search value")
+    const [searchValue, setSearchValue] = useState("")
 
     const handleInputChange = (event) => {
         setSearchValue(event.target.value)
     }
+
+    const handleClearClick = () =>{
+        setSearchValue("")
+    }
+
+    const shouldDisplayButton = searchValue.length > 0
+    console.log(shouldDisplayButton)
     return <div>
         <input type="text" value={searchValue} onChange={handleInputChange}/>
+        {shouldDisplayButton && <button onClick={handleClearClick}>clear</button>}
+        
     </div>
 }
 
